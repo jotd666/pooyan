b72dc98bcf2f81fd15061b6bfa74c080571f2624
Jean-François Fabre <jotd666@gmail.com>
2024-11-30 23:50:16 +0100

    fixed boss wolf bug

--- tools/extract_unknown_jump_table_functions.py
+++ tools/extract_unknown_jump_table_functions.py
@@ -3,6 +3,7 @@ import os,re
 this_dir = os.path.dirname(os.path.abspath(__file__))
 
 jere = re.compile(r"\s+\.long\s+l_(\w\w\w\w)\b",flags=re.I)
+jekre = re.compile(r"\s+\.long\s+(\w+)",flags=re.I)
 
 with open(os.path.join(this_dir,"..","src","pooyan.68k")) as f:
     lines = list(f)
@@ -13,7 +14,10 @@ for line in lines:
     m = jere.match(line)
     if m:
         result.add(int(m.group(1),0x10))
-
+    else:
+        m = jekre.match(line)
+        if m:
+            print("Known: {}".format(m.group(1)))
 with open(r"K:\Emulation\MAME\bkpts","w") as f:
     f.write("bpclear\n")
     for r in sorted(result):
